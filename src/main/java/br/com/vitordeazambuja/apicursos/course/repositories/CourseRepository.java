b948a8b63aa9b87a2e96610f2254d99e61ec5572
Vitor <vitordeazambuja@gmail.com>
2026-01-27 20:10:28 -0300

    feat(course): adicionar repositório JPA para cursos

---
+++ src/main/java/br/com/vitordeazambuja/apicursos/course/repositories/CourseRepository.java
@@ -0,0 +1,12 @@
+package br.com.vitordeazambuja.apicursos.course.repositories;
+
+import br.com.vitordeazambuja.apicursos.course.entities.CourseEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface CourseRepository extends JpaRepository<CourseEntity, UUID> {
+    Optional<CourseEntity> findByName(String name);
+    Optional<CourseEntity> findByCategory(String category);
+}
